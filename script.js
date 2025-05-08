13dbf966ef274a0dbf9dc77e8522e8eaea085f5f
Oleksandr <sasha.chernetskyi09@gmail.com>
2025-05-08 21:34:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ let scenes = document.querySelectorAll('.scene')
 btns.forEach(function(btn){
     btn.addEventListener('click', function(){
         let nextScene = btn.getAttribute('data-target');
+        if (nextScene == "random") { //Випадковий вибір сцени(використане тільки один раз)
+            let rand_num = Math.floor(Math.random() * 2);
+            nextSceneList = ['scene4a', 'scene4b'];
+            nextScene = nextSceneList[rand_num];
+
+        }
+        if( nextScene == 'scene11a') { //Запуск модального віконця після проходження основної лінії квесту
+            setTimeout(function(){
+                Swal.fire({
+                    title: "Ви пройшли квест!",
+                    icon: "success",
+                    draggable: true
+                  });
+            }, 3000)
+        }
         scenes.forEach(function(scene){
             scene.classList.remove('active')
         })
